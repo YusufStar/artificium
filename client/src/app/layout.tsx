a274e83a1d74346fe4484a232b3ef16e71ae8f9b
yusuf <ysfyldz0077@gmail.com>
2024-03-08 00:41:40 +0300

    fix font

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,13 +1,5 @@
 import "@/styles/globals.css";
-import { Plus_Jakarta_Sans } from "next/font/google";
-
-import { cn } from "@/lib/utils";
 import AuthContainer from "@/components/provider/AuthContainer";
-
-export const jakarta = Plus_Jakarta_Sans({
-  subsets: ["latin", "latin-ext", "cyrillic-ext"],
-  display: "swap",
-});
 
 export default function RootLayout({
   children,
@@ -18,10 +10,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head />
       <body
-        className={cn(
-          "min-h-screen bg-nobbleBlack-100 text-white font-sans antialiased",
-          jakarta.className
-        )}
+        className="min-h-screen bg-nobbleBlack-100 text-white font-sans antialiased"
       >
         <AuthContainer>{children}</AuthContainer>
       </body>
