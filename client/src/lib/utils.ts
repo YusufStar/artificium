fea3d39cfba2183e45bb1b4f3d97e9ba27f4d838
yusuf <ysfyldz0077@gmail.com>
2024-03-10 22:51:06 +0300

    Update utils.ts

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { usePathname } from "next/navigation";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function isActivePath(path: string, pathname: string) {
+  return path === pathname;
 }
