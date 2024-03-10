15ffb52585c4c4eef15e26b85f70d590e1fdde2b
yusuf <ysfyldz0077@gmail.com>
2024-03-10 22:50:41 +0300

    Create navigation-button.tsx

---
+++ client/src/components/ui/navigation-button.tsx
@@ -0,0 +1,32 @@
+import { cn, isActivePath } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+type Props = {
+  label: string;
+  icon?: LucideIcon;
+  path: string;
+};
+
+const NavigationButton = (props: Props) => {
+  const pathname = usePathname() as string;
+
+  return (
+    <Link
+      className={cn(
+        "border-t rounded-8 p-14 gap-16 flex items-center transition-all duration-200 ease-in-out",
+        isActivePath(pathname, props.path) ? "bg-[#131619] border-glassStroke" : "bg-nobbleBlack-800 border-t-nobbleBlack-800"
+      )}
+      href={props.path}
+    >
+      {props.icon && <props.icon color="#686B6E" className="w-[1.25rem] h-[1.25rem] drop-shadow-icon" />}
+      <span className="font-semibold text-14 text-nobbleBlack-100">
+        {props.label}
+      </span>
+    </Link>
+  );
+};
+
+export default NavigationButton;
