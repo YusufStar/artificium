af4fc7f26369026561511f844c4a150a11aa761a
yusuf <ysfyldz0077@gmail.com>
2024-03-13 20:30:06 +0300

    Update layout.tsx

--- client/src/app/(Main)/layout.tsx
+++ client/src/app/(Main)/layout.tsx
@@ -1,16 +1,22 @@
+"use client";
 import Sidebar from "@/components/MainLayout/Sidebar";
 import TopBar from "@/components/MainLayout/TopBar";
 import SocketProvider from "@/components/provider/SocketProvider";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
   return (
     <SocketProvider>
       <div className="h-screen w-full flex gap-12 p-12">
         <Sidebar />
 
         <div className="flex-1 flex flex-col">
-          <TopBar />
+          {pathname === "/billing" || pathname === "/search" ? null : (
+            <TopBar />
+          )}
 
           <div className="flex-1">{children}</div>
         </div>
