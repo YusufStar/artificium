7a807f74d846ea0e2e6902f74529ecbf56a72566
yusuf <ysfyldz0077@gmail.com>
2024-03-14 23:14:35 +0300

    Update middleware.ts

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,18 +1,27 @@
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
 
-export function middleware(req, event) {
-  const token = req.cookies.token;
+export function middleware(request: NextRequest) {
+  const token = request.cookies?.token;
 
   if (token) {
     try {
-      const decoded = jwt.verify(token, process.env.JWT_KEY);
-      
+      const decoded = jwt.verify(token, process.env.JWT_KEY as string) as {
+        id: string;
+        email: string;
+        organization: any;
+      };
+
       // Kullanıcı varsa ve organizasyon bilgisi yoksa sadece create ve join sayfalarına yönlendir
-      if (!decoded.organization && (req.url !== "/organization/create" && req.url !== "/organization/join")) {
+      if (
+        !decoded.organization &&
+        request.url !== "/organization/create" &&
+        request.url !== "/organization/join"
+      ) {
         return NextResponse.redirect("/organization/join");
       }
-      
+
       // Kullanıcı varsa ve organizasyon bilgisi varsa herhangi bir yönlendirme yapma
       if (decoded.organization) {
         return NextResponse.next();
@@ -23,7 +32,11 @@ export function middleware(req, event) {
     }
   } else {
     // Token yoksa sadece login ve register sayfalarına erişime izin ver
-    if (req.url !== "/login" && req.url !== "/register" && req.url !== "/verify-email") {
+    if (
+      request.url !== "/login" &&
+      request.url !== "/register" &&
+      request.url !== "/verify-email"
+    ) {
       return NextResponse.redirect("/login");
     }
   }
